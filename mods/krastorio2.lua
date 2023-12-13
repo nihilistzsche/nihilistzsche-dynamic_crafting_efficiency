12b48ea6add74ee5421fe7cefac9f15b7f386e4f
Nihilistzsche <nihilistzsche@gmail.com>
2023-12-12 17:18:45 -0800

    Add the rest of the krastorio2 items.

--- mods/krastorio2.lua
+++ mods/krastorio2.lua
@@ -144,3 +144,225 @@ k2_se_ce_add_recipe("enriched-copper-plate", {
         level = 3,
     },
 })
+k2_se_ce_add_recipe("automation-core", {
+    name = "Automation Core efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/automation-core.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 1
+    },
+})
+k2_se_ce_add_recipe("electronic-components", {
+    name = "Electronic Components efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items-with-variations/electronic-components/electronic-components.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 2
+    },
+})
+k2_se_ce_add_recipe("imersium-plate", {
+    name = "Imersium Plate efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/imersium-plate.png",
+    crafting = {
+        efficiency = 20,
+        category = "smelting",
+    },
+    research = {
+        level  = 5
+    },
+})
+k2_se_ce_add_recipe("imersium-gear-wheel", {
+    name = "Imersium Gear Wheel efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/imersium-gear-wheel.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 5
+    },
+})
+k2_se_ce_add_recipe("imersium-beam", {
+    name = "Imersium Beam efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/imersium-beam.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 5
+    },
+})
+k2_se_ce_add_recipe("imersite-crystal", {
+    name = "Imersite Crystal efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items-with-variations/imersite-crystal/imersite-crystal.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 6,
+    },
+})
+k2_se_ce_add_recipe("ai-core", {
+    name = "AI Core efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/ai-core.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 6
+    },
+})
+k2_se_ce_add_recipe("pollution-filter", {
+    name = "Pollution Filter efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/pollution-filter.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 6
+    },
+})
+k2_se_ce_add_recipe("improved-pollution-filter", {
+    name = "Improved Pollution Filter efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/improved-pollution-filter.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 6
+    },
+})
+k2_se_ce_add_recipe("empty-dt-fuel", {
+    name = "Empty DT Fuel efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/empty-dt-fuel.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("dt-fuel", {
+    name = "DT Fuel efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/dt-fuel.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("energy-control-units", {
+    name = "Energy Control Units efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/energy-control-units.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("matter-stabilizer", {
+    name = "Matter Stabilizer efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/matter-stabilizer.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("charged-matter-stabilizer", {
+    name = "Charged Matter Stabilizer efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/charged-matter-stabilizer.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("matter-cube", {
+    name = "Matter Cube efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/matter-cube.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("empty-antimatter-fuel-cell", {
+    name = "Empty Antimatter Fuel Cell efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/empty-antimatter-fuel-cell.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("charged-antimatter-fuel-cell", {
+    name = "Charged Antimatter Fuel Cell efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/charged-antimatter-fuel-cell.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("inserter-parts", {
+    name = "Inserter Parts efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items-with-variations/inserter-parts/inserter-parts.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 1
+    },
+})
+k2_se_ce_add_recipe("gps-satellite", {
+    name = "GPS Satellite efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/gps-satellite.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+k2_se_ce_add_recipe("teleportation-gps-module", {
+    name = "Teleportation GPS Module efficiency",
+    max = 15,
+    icon = "__Krastorio2Assets__/icons/items/teleportation-gps-module.png",
+    crafting = {
+        efficiency = 10,
+    },
+    research = {
+        level = 7
+    },
+})
+
