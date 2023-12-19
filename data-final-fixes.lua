79d7127e88551f3333d04d62f7735a78567995ab
Nihilistzsche <nihilistzsche@gmail.com>
2023-12-18 21:00:29 -0800

    Remove non-dmr ingredients from replications and the replication lab.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,5 @@
+local function starts_with(str, start) return str:sub(1, #start) == start end
+
 if mods["bzaluminum"] then
     local util = require("__bzaluminum__.data-util")
     local item = "automation-core"
@@ -8,4 +10,25 @@ if mods["bzaluminum"] then
     for i = 1, 15 do
         util.replace_ingredient("ce-" .. item .. "-" .. i, "iron-plate", "aluminum-plate")
     end
+end
+if mods["dark-matter-replicators-18"] then
+    for tech, tbl in pairs(data.raw.technology) do
+        if starts_with(tech, "dmr18-repl-") then
+            local new_ingredients = {}
+            for _, i in ipairs(tbl.unit.ingredients) do
+                if starts_with(i[1], "dmr18-") then
+                    table.insert(new_ingredients, i)
+                end
+            end
+            tbl.unit.ingredients = new_ingredients
+        end
+    end
+    local repl_lab = data.raw.lab["dmr18-replication-lab"]
+    local new_inputs = {}
+    for _, input in ipairs(repl_lab.inputs) do
+        if starts_with(input, "dmr18-") then
+            table.insert(new_inputs, input)
+        end
+    end
+    repl_lab.inputs = new_inputs
 end
